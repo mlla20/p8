3e75de759c7799b8461547abfc40a713803c4261
mlla20 <70723224+mlla20@users.noreply.github.com>
2024-03-14 00:37:12 +0100

    Dataset og load af data

--- main.py
+++ main.py
@@ -1,3 +1,27 @@
 import torch
+import torch.nn as nn
+import torchaudio
+from torch.utils.data import Dataset
+import torchvision.transforms
 import speechbrain
+import matplotlib.pyplot as plt
 
+# Load data
+# Link til dataset doc: https://pytorch.org/audio/main/generated/torchaudio.datasets.LIBRISPEECH.html#torchaudio.datasets.LIBRISPEECH
+data =  torchaudio.datasets.LIBRISPEECH(root ='./LibreSpeech', url = 'dev-clean', download= True) 
+#print(data[1][0].shape)
+
+
+# Define model 
+# Define loss and optimizer 
+
+# Train model
+lr = 0.01
+epochs = 1000
+
+#for epoch in range(epochs):
+    # Compute prediction
+
+    # Compute Loss
+    # Computer Gradient 
+    # Update weights 
